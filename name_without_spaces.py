9f1ae1091a0ef43064f6d92d6d90aa07a609a305
unknown <ramosyazmien2@gmail.com>
2025-03-20 11:46:09 +0800

    get user name and print without spaces

--- name_without_spaces.py
+++ name_without_spaces.py
@@ -1,4 +1,6 @@
-# pseudocode
 # input full name with spaces
+user_name = str(input("Enter your name with spaces: "))
 # remove the left padded spaces in name string using lstrip.()
+user_name = user_name.lstrip()
 # print name
+print(user_name)
