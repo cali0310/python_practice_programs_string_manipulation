bd3cd450870cbf3e30fafc3eba1b0a551228fe42
unknown <ramosyazmien2@gmail.com>
2025-03-20 13:10:29 +0800

    print number of words

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -2,3 +2,7 @@
 # input complete statement
 # count words in statement
 # print number of words
+
+enter_statement = str(input("Enter a complete statement: "))
+word_count = len(enter_statement.split())
+print("Number of words: ", word_count)
