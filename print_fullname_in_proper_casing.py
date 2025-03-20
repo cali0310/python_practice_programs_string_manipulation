2873e00247e2f5e8894bf3ea9a5d8f694bdec451
unknown <ramosyazmien2@gmail.com>
2025-03-20 12:26:26 +0800

    pseudocode

---
+++ print_fullname_in_proper_casing.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+# input fullname
+# set fullname to title case
+# print fullname
