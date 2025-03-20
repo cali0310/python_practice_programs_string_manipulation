445b535934502aeb2efcf1f7ba4a027d3b88de0d
unknown <ramosyazmien2@gmail.com>
2025-03-20 13:45:11 +0800

    print name in snake case

--- print_fullname_in_snake_case.py
+++ print_fullname_in_snake_case.py
@@ -2,3 +2,7 @@
 # input fullname
 # set fullname to snake casing
 # print fullname
+
+fullname = str(input("Enter your fullname in incorrect casing: ").lower())
+snake_case = fullname.replace(" ", "_")
+print(snake_case)
