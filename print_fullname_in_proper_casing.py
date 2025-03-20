ea97c2385e938b19bf11fbcf538c0d6f6b47c343
unknown <ramosyazmien2@gmail.com>
2025-03-20 12:31:37 +0800

    print name in proper case

--- print_fullname_in_proper_casing.py
+++ print_fullname_in_proper_casing.py
@@ -1,7 +1,8 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 # input fullname
-fullname = str(input("Enter your fullname: "))
 # set fullname to title case
-fullname = fullname.title()
 # print fullname
+
+fullname = str(input("Enter your fullname: "))
+fullname = fullname.title()
 print(fullname)
